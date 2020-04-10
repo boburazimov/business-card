0b6cf1b6753ad37f0b6506949be664a1d1e4eed2
BA0809 <ard_admin@mail.ru>
2020-04-10 23:08:21 +0500

    10/04/2020 Add Entity: User, Role, AbsEntity

--- business-card-server/src/main/java/uz/app/entity/User.java
+++ business-card-server/src/main/java/uz/app/entity/User.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import uz.app.entity.template.AbsEntity;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
@@ -14,7 +17,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "users")
-public class User extends AbsEntity {
+public class User extends AbsEntity implements UserDetails {
 
     @Column(nullable = false, unique = true)
     private String email;
@@ -37,4 +40,38 @@ public class User extends AbsEntity {
             inverseJoinColumns = {@JoinColumn(name = "role_id")})
     private List<Role> roles;
 
+    private boolean accountNonExpired = true;
+    private boolean accountNonLocked = true;
+    private boolean credentialsNonExpired = true;
+    private boolean enabled = true;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.phoneNumber;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return this.accountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return this.accountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return this.credentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.enabled;
+    }
 }
