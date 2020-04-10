6525ca7ab4e7f189101d407b5933968a27c44bb3
BA0809 <ard_admin@mail.ru>
2020-04-10 23:29:20 +0500

    10/04/2020 Add Client-Side

---
+++ app-business-card-server/src/main/java/uz/bus/card/AppBusinessCardApplication.java
@@ -0,0 +1,13 @@
+package uz.bus.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBusinessCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppBusinessCardApplication.class, args);
+	}
+
+}
---
+++ app-business-card-server/src/test/java/uz/bus/card/AppBusinessCardApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.bus.card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppBusinessCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
