899af23e953c3a935050429801fdaaa5028a51f4
BA0809 <ard_admin@mail.ru>
2020-04-13 01:24:37 +0500

    12-04-2020

--- business-card-client/src/App.js
+++ business-card-client/src/App.js
@@ -5,6 +5,7 @@ function App() {
     return (
         <div className="App">
             <MainNavBar/>
+            <div id="scalemaxmin">HOME</div>
             <div style={{minHeight: 800 + 'em'}}/>
         </div>
     );
--- business-card-client/src/component/MainNavBar.js
+++ business-card-client/src/component/MainNavBar.js
@@ -5,7 +5,7 @@ function MainNavBar() {
     return (
         <div className="bg-img">
             <Navbar expand="lg" className="sticky-top">
-                <Navbar.Brand href="#home">
+                <Navbar.Brand href="#home" id="navbarBrand">
                     <img
                         alt=""
                         src="img/logo.svg"
@@ -17,12 +17,15 @@ function MainNavBar() {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Form inline className="mr-auto">
-                        <FormControl type="text" placeholder="Search" className="border-dark bg-transparent mr-sm-2" />
-                        <Button variant="outline-success">Search</Button>
+                        <FormControl type="text" placeholder="Search" className="border-danger bg-transparent mr-sm-2" />
+                        <Button variant="outline-danger">Search</Button>
                     </Form>
                     <Nav >
                         <Button variant="outline-success" className="mr-sm-2">Home</Button>
-                        <Button variant="outline-success" className="mr-sm-2">Contact</Button>
+                        <Button variant="outline-success" className="mr-sm-2">ABOUT</Button>
+                        <Button variant="outline-success" className="mr-sm-2">WORK</Button>
+                        <Button variant="outline-success" className="mr-sm-2">BLOG</Button>
+                        <Button variant="outline-success" className="mr-sm-2">CONTACT</Button>
                         <Button variant="info" className="mr-sm-2">Login</Button>
                     </Nav>
                 </Navbar.Collapse>
